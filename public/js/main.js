f5e5e83948468b5ccb1ba30e7f9e39c582e1cfe5
felipefforigua <felipefforigua@gmail.com>
2024-09-03 22:24:17 -0500

    Se mejoro la vista principal con carrusel

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-const $cards = document.querySelectorAll('.cards'),
-$template = document.getElementById('template-card').content,
-$fragment = document.createDocumentFragment(),
-cardContent = [
-    
-];
